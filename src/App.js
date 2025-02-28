311e9b08bbf473c3b6e19744f8b7741258419f83
Knarik Muradyan <muradyan.knarik96@gmail.com>
2025-02-28 20:25:38 +0400

    Made Forms in App.js  like a components

--- src/App.js
+++ src/App.js
@@ -25,7 +25,11 @@ export default function App() {
     <div className="app">
       <div className="sidebar">
         <FriendsList />
+        <FormaddFriend />
+        <Button>Add friend</Button>
       </div>
+      <FormSplitBill />
+
     </div>
   )
 };
@@ -64,7 +68,52 @@ function Friend({ friend }) {
           You and {friend.name} are even
         </p>}
 
-    <button className="button">Select</button>
+      <Button>Select</Button>
     </li>
   )
+}
+function Button({ children }) {
+
+  return <button className="button">{children}</button>
+
+}
+
+function FormaddFriend() {
+
+  return <form className="form-add-friend">
+
+    <label>👥Friend name</label>
+    <input type="text" />
+
+    <label>🌆Image URL</label>
+    <input type="text" />
+
+    <Button>Add</Button>
+  </form>
+}
+
+function FormSplitBill() {
+  return <form className="form-split-bill">
+    <h2> Split a bill with X</h2>
+
+    <label>💰Bill value</label>
+    <input type="text" />
+
+    <label>🧍‍♀️Your expense</label>
+    <input type="text" />
+    
+    <label>👥X-s expense</label>
+    <input type="text" disabled />
+
+    <label>🤑Who is paying the bill?</label>
+    <select>
+      <option value="user">You</option>
+      <option value="friend">X</option>
+    </select>
+
+    <Button>Split bill</Button>
+
+
+
+  </form>
 }
